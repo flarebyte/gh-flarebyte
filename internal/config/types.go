aef634399ccd86623703432053aea53ad2efb4b6
Olivier Huin <olih@users.noreply.github.com>
2026-05-01 09:17:42 +0100

    split config

---
+++ internal/config/types.go
@@ -0,0 +1,58 @@
+package config
+
+import "regexp"
+
+const DefaultPath = ".gh-flarebyte.cue"
+
+type Config struct {
+	Project    ProjectConfig    `json:"project"`
+	Sync       SyncConfig       `json:"sync"`
+	Repository RepositoryConfig `json:"repository"`
+	Build      BuildConfig      `json:"build"`
+	Release    ReleaseConfigRaw `json:"release"`
+}
+
+type ProjectConfig struct {
+	Org  string `json:"org"`
+	Repo string `json:"repo"`
+}
+
+type SyncConfig struct {
+	Mode string `json:"mode"`
+}
+
+type RepositoryConfig struct {
+	Description   string        `json:"description"`
+	DefaultBranch string        `json:"defaultBranch"`
+	Homepage      string        `json:"homepage"`
+	Visibility    string        `json:"visibility"`
+	Template      bool          `json:"template"`
+	Topics        []string      `json:"topics"`
+	Labels        []LabelConfig `json:"labels"`
+}
+
+type LabelConfig struct {
+	Name        string `json:"name"`
+	Color       string `json:"color"`
+	Description string `json:"description"`
+}
+
+type BuildConfig struct {
+	Language     string   `json:"language"`
+	OutputDir    string   `json:"outputDir"`
+	ChecksumFile string   `json:"checksumFile"`
+	Targets      []string `json:"targets"`
+}
+
+type ReleaseConfigRaw struct {
+	VersionSource        string `json:"versionSource"`
+	TagPrefix            string `json:"tagPrefix"`
+	NotesMode            string `json:"notesMode"`
+	ReleaseNotesFilePath string `json:"releaseNotesFilePath,omitempty"`
+	ArtifactDir          string `json:"artifactDir"`
+	IncludeChecksums     bool   `json:"includeChecksums"`
+}
+
+var targetPattern = regexp.MustCompile(`^(linux|darwin|windows)-(amd64|arm64)$`)
+var quotedStringPattern = regexp.MustCompile(`"([^"]+)"`)
+var labelObjectPattern = regexp.MustCompile(`(?s)\{([^{}]*)\}`)
